a9b81685f76c57405e3d64bd90a06e746690c70c
Fernanda Magalhaes <ferlmagalhaes@gmail.com>
2026-05-01 19:10:01 -0300

    fix: remove coral-care basename from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/coral-care">
+      <BrowserRouter basename="/">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
